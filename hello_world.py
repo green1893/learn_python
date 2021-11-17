0809e2f4a333c6a5966b4e06b9809967b7ce7a0c
green1893 <green1893@163.com>
2021-11-17 16:48:03 +0800

    fisrt commit

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# 打印
+print("Hello World!")
+
+# 定义变量
+name = "ZHAO"
+print("Nice to meet you " + name)
+
+# 变量值修改
+name = "GREEN"
+print("How do you do " + name)
---
+++ name.py
@@ -0,0 +1,18 @@
+# 字符串
+name = "steve jobs"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# 去除空格
+city = " 浙江省杭州市 "
+street = "余杭西溪"
+# 去除尾部空格
+print(city.rstrip() + street)
+# 去除头尾空格
+print(city.strip() + street)
+
+# 数学计算
+price = 1.89
+num = 20
+print(price * num)
