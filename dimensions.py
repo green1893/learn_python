774d35d8c3c26cc350f66429d6dccc45332eb596
green1893 <green1893@163.com>
2021-11-18 09:20:37 +0800

    元组定义和操作

---
+++ dimensions.py
@@ -0,0 +1,15 @@
+dimensions = (0,12)
+print(dimensions[1])
+
+# TypeError，不支持修改
+# dimensions[1] = 1
+
+for num in dimensions:
+    print(num)
+
+# 给元组重新赋予数组值
+dimensions = (200,100)
+print(dimensions)
+
+# 编码指南 PEP 8
+# https://python.org/dev/peps/pep-0008/
